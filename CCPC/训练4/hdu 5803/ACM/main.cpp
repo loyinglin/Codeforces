c566b2bdc416b1df5c7227d622d2ae6d3f29b6c5
林伟池 <loying@foxmail.com>
2016-11-02 09:29:23 +0800

    hdu 5803

---
+++ CCPC/训练4/hdu 5803/ACM/main.cpp
@@ -0,0 +1,44 @@
+//
+//  main.cpp
+//  ACM
+//
+//  Created by 林伟池 on 16/9/4.
+//  Copyright © 2016年 林伟池. All rights reserved.
+/************************** 题解 **********************
+ 题目链接：
+ 题目大意：
+ 
+ 
+ 
+ 
+ 题目解析：
+ 
+ 
+ ************************* 题解 ***********************/
+#include<cstdio>
+#include<cmath>
+#include<stack>
+#include<map>
+#include<set>
+#include<queue>
+#include<deque>
+#include<string>
+#include<utility>
+#include<sstream>
+#include<cstring>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+typedef long long lld;
+
+const int Max = 1000100100;
+
+
+int main(int argc, const char * argv[]) {
+    int t = 1000;
+    cin >> t;
+    while (t--) {
+    
+    }
+}
