b1db575045a5029e73680b03a85cf5cf4a9cc619
林伟池 <loying@foxmail.com>
2016-11-04 10:29:48 +0800

    网络流~

--- CCPC/测试/ACM/main.cpp
+++ CCPC/测试/ACM/main.cpp
@@ -1,181 +1,182 @@
-#include<iostream>
-#include<cstdio>
-#include<cstring>
-#include<queue>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
 using namespace std;
-#define INF 110000000
-#define NN 600
-#define MM 50100
+#define L(i) i<<1
+#define R(i) i<<1|1
+#define INF  0x3f3f3f3f
+#define pi acos(-1.0)
+#define eps 1e-9
+#define maxn 100100
+#define MOD 1000000007
 
+const int MAXN = 100010;
+const int MAXM = 400010;
+struct Edge
+{
+    int to,next,cap,flow;
+}edge[MAXM];
+int tot,n,m,N,l;
+int head[MAXN];
+int gap[MAXN],dep[MAXN];
+int pre[MAXN],cur[MAXN];
+int cost[220],TIME[220],tTIME[220];
+int val[220],shop[220][220];
 
-struct edge{int u,v,c;}ed[MM];//MM要为连接关系数量的两倍
-int fi[NN],ne[MM],te,lev[NN];
-
-int pe[NN],cap[NN],tc[NN],sta[NN];//模拟栈用数组
-
-void init(){
-    memset(fi,-1,sizeof(fi));
-    te=0;
+void init()
+{
+    tot = 0;
+    memset(head,-1,sizeof(head));
 }
-
-void insert(int u,int v,int c){
-    ed[te].u=u;ed[te].v=v;ed[te].c=c;
-    ne[te]=fi[u];fi[u]=te++;
-    ed[te].u=v;ed[te].v=u;ed[te].c=0;
-    ne[te]=fi[v];fi[v]=te++;
+void add_edge(int u,int v,int w,int rw = 0)
+{
+    edge[tot].to = v;
+    edge[tot].cap = w;
+    edge[tot].flow = 0;
+    edge[tot].next = head[u];
+    head[u] = tot++;
+    edge[tot].to = u;
+    edge[tot].cap = rw;
+    edge[tot].flow = 0;
+    edge[tot].next = head[v];
+    head[v] = tot++;
 }
-
-int dinic_bfs(int s,int t){
-    memset(lev,0,sizeof(lev));
-    lev[s]=1;
-    queue<int> q;
-    q.push(s);
-    int u,v,e,c;
-    while(!q.empty()){
-        u=q.front();q.pop();
-        for(e=fi[u];e!=-1;e=ne[e]){
-            v=ed[e].v;c=ed[e].c;
-            if (!lev[v]&&c){
-                lev[v]=lev[u]+1;
-                q.push(v);
+int sap(int start,int en)
+{
+    memset(gap,0,sizeof(gap));
+    memset(dep,0,sizeof(dep));
+    memcpy(cur,head,sizeof(head));
+    int u = start;
+    pre[u] = -1;
+    gap[0] = N;
+    int ans = 0;
+    while(dep[start] < N)
+    {
+        if(u == en)
+        {
+            int Min = INF;
+            for(int i = pre[u]; i != -1; i = pre[edge[i^1].to])
+                if(Min > edge[i].cap - edge[i].flow)
+                    Min = edge[i].cap - edge[i].flow;
+            for(int i = pre[u]; i != -1; i = pre[edge[i^1].to])
+            {
+                edge[i].flow += Min;
+                edge[i^1].flow -= Min;
+            }
+            u = start;
+            ans += Min;
+            continue;
+        }
+        int flag = 0;
+        int v;
+        for(int i = cur[u]; i != -1; i = edge[i].next)
+        {
+            v = edge[i].to;
+            if(edge[i].cap - edge[i].flow && dep[v]+1 == dep[u])
+            {
+                flag = 1;
+                cur[u] = pre[v] = i;
+                break;
             }
         }
-        if (u==t) break;
-    }
-    return lev[t];
-}
-
-int dinic_dfs1(int u,int t,int cap){//直接深搜
-    int tc=cap;
-    if (u==t) return cap;
-    int v,e,c,tf;
-    for(e=fi[u];e!=-1&&tc;e=ne[e]){
-        v=ed[e].v;
-        c=ed[e].c;
-        if (lev[v]==lev[u]+1&&c>0){
-            tf=dinic_dfs1(v,t,min(tc,c));
-            tc-=tf;
-            ed[e].c-=tf;
-            ed[e^1].c+=tf;
+        if(flag)
+        {
+            u = v;
+            continue;
         }
-    }
-    if (e==-1) lev[u]=-10;//重要优化
-    return cap-tc;
-}
-
-
-
-int dinic_dfs(int s,int t,int tn){//模拟栈深搜
-    int top=0,i,flow=0;
-    sta[++top]=s;
-    cap[s]=tc[s]=INF;
-    pe[s]=fi[s];
-    int u,v,c,tf;
-    while(top){
-        u=sta[top];
-        if (u!=t)
-            for(int &e=pe[u];e!=-1&&tc[u];e=ne[e]){
-                v=ed[e].v;c=ed[e].c;
-                if (lev[v]==lev[u]+1&&c){
-                    sta[++top]=v;pe[v]=fi[v];tc[v]=cap[v]=min(c,tc[u]);
-                    break;
-                }
+        int Min = N;
+        for(int i = head[u]; i != -1; i = edge[i].next)
+            if(edge[i].cap - edge[i].flow && dep[edge[i].to] < Min)
+            {
+                Min = dep[edge[i].to];
+                cur[u] = i;
             }
-        else tc[u]=0;
-        if (tc[u]==0||pe[u]==-1) {
-            tf=cap[u]-tc[u];
-            if (pe[u]==-1) lev[u]=-10;//重要优化
-            top--;
-            if (top){
-                tc[sta[top]]-=tf;
-                ed[pe[sta[top]]].c-=tf;
-                ed[pe[sta[top]]^1].c+=tf;
-                pe[sta[top]]=ne[pe[sta[top]]];
-            }
-            else flow+=tf;
-        }
+        gap[dep[u]]--;
+        if(!gap[dep[u]])
+            return ans;
+        dep[u] = Min + 1;
+        gap[dep[u]]++;
+        if(u != start)
+            u = edge[pre[u]^1].to;
     }
-    return flow;
+    return ans;
 }
-
-
-
-int dinic(int s,int t,int tn){//起点终点相等需要特判
-    int flow=0;
-    int tf=0;
-    while(dinic_bfs(s,t)){
-        flow+=dinic_dfs(s,t,t+1);
-    }
-    return flow;
-}
-
-#define KK 110
-
-int n,m,tte,k;
-int g[KK][KK];
-int s,t;
-
-
-void input(int tte){
-    int i;
-    int a,b;
-    memset(g,0,sizeof(g));
-    for(i=1;i<=tte;++i){
-        scanf("%d%d",&a,&b);
-        g[b][a]=1;
-    }
-}
-
-int work(int now){
+int solve(int x)
+{
     init();
-    s=0;t=3*m+1;
-    int i,j;
-    for(i=1;i<=m;++i){
-        insert(s,i,now);
-        insert(i,i+m,k);
-    }
-    for(i=1;i<=m;++i){
-        for(j=1;j<=n;++j){
-            if (g[i][j]==1){
-                insert(i,j+2*m,1);
-            }
-            else {
-                insert(i+m,j+2*m,1);
-            }
+    int vis[220] = {0};
+    int t = tTIME[x],sum = 0;
+    for(int i = 1; i <= m; i++)
+        for(int j = 1; j <= shop[i][0]; j++)
+            add_edge(i,shop[i][j]+m,INF);
+    for(int i = 1; i <= n; i++)
+        if(TIME[i] <= t)
+        {
+            vis[i] = 1;
+            add_edge(m+i,m+n+1,cost[i]);
+        }
+    for(int i = 1; i <= m; i++)
+    {
+        int flag = 1;
+        for(int j = 1; j <= shop[i][0]; j++)
+            if(!vis[shop[i][j]])
+                flag = 0;
+        if(flag)
+        {
+            sum += val[i];
+            add_edge(0,i,val[i]);
         }
     }
-    for(i=1;i<=n;++i){
-        insert(i+2*m,t,now);
-    }
-    int flow = dinic(s,t,t+1);
-    if (flow==now*n) return 1;
-    else return 0;
-}
-
-
-
-
-int main(){
-    int tcas,cas;
-    scanf("%d",&tcas);
-    for(cas=1;cas<=tcas;++cas){
-        scanf("%d%d%d%d",&n,&m,&tte,&k);
-        input(tte);
-        int l=0,r=m,ans=0,mid;
-        while(l<=r){
-            mid=(l+r)/2;
-            if (work(mid)) {
-                ans=max(ans,mid);
-                l=mid+1;
-            }
-            else r=mid-1;
-        }
-        printf("Case %d: %d\n",cas,ans);
+    int ans = sap(0,m+n+1);
+    ans = sum - ans;
+    if(ans >= l)
+    {
+        printf("%d %d\n",t,ans);
+        return 1;
     }
     return 0;
 }
-
-
-
-
-
+int main()
+{
+    //freopen("in.txt","r",stdin);
+    //freopen("out.txt","w",stdout);
+    int t,C = 1;
+    scanf("%d",&t);
+    while(scanf("%d%d%d",&n,&m,&l) != EOF)
+    {
+        for(int i = 1; i <= n; i++)
+        {
+            scanf("%d%d",&cost[i],&TIME[i]);
+            tTIME[i] = TIME[i];
+        }
+        sort(tTIME+1,tTIME+n+1);
+        for(int i = 1; i <= m; i++)
+        {
+            scanf("%d%d",&val[i],&shop[i][0]);
+            for(int j = 1; j <= shop[i][0]; j++)
+                scanf("%d",&shop[i][j]);
+        }
+        N = m + n + 2;
+        printf("Case #%d: ",C++);
+        int flag = 0;
+        for(int i = 1; i <= n; i++)
+            if(solve(i))
+            {
+                flag = 1;
+                break;
+            }
+        if(!flag)
+            printf("impossible\n");
+    }
+    return 0;
+}
---
+++ CCPC/训练4/hdu 5855/ACM/main.cpp
@@ -0,0 +1,180 @@
+//
+//  main.cpp
+//  ACM
+//
+//  Created by 林伟池 on 16/9/4.
+//  Copyright © 2016年 林伟池. All rights reserved.
+/************************** 题解 **********************
+ 题目链接：
+ 题目大意：
+ 
+ 
+ 
+ 题目解析：
+ 
+ 
+ 
+ ************************* 题解 ***********************/
+#include<cstdio>
+#include<cmath>
+#include<stack>
+#include<map>
+#include<set>
+#include<queue>
+#include<deque>
+#include<string>
+#include<utility>
+#include<sstream>
+#include<cstring>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+typedef long long lld;
+const int N = 420, M = 200000, inf = 100100100; //注意修改点的范围 N所有点的范围
+struct Edge{
+    int s, t, f, id;
+    int next;
+}e[M];
+
+int pre[N], tpre[N], dis[N], cnt;
+int pro[N], Time[N], stTime[N], pay[N], vis[N];
+vector<int> g[N];
+void addedge(int s, int t, int f, int id){ //有上下界的最大流模板
+    //    cout << s << " TO " << t << " FLOW " << f << endl;
+    e[cnt].s = s, e[cnt].t = t, e[cnt].f = f, e[cnt].id = id, e[cnt].next = pre[s];
+    pre[s] = cnt++;
+    e[cnt].s = t, e[cnt].t = s, e[cnt].f = 0, e[cnt].id = 0, e[cnt].next = pre[t];
+    pre[t] = cnt++;
+}
+int bfs(int s, int t){
+    memset(dis, 0, sizeof(dis));
+    queue<int> q;
+    dis[s] = 1;
+    q.push(s);
+    while(!q.empty()){
+        int id = q.front();
+        q.pop();
+        for(int i = pre[id]; i != -1; i = e[i].next)
+            if(dis[e[i].t] == 0 && e[i].f > 0){
+                dis[e[i].t] = dis[id] + 1;
+                q.push(e[i].t);
+            }
+    }
+    return dis[t] != 0;
+}
+int dfs(int s, int t, int cur_flow){
+    int lev = cur_flow;
+    if(s == t) return cur_flow;
+    for(int &i = tpre[s]; i != -1; i = e[i].next)
+        if(dis[e[i].t] == dis[s] + 1 && e[i].f > 0 && lev > 0){
+            int temp = dfs(e[i].t, t, min(lev, e[i].f));
+            e[i].f -= temp;
+            e[i^1].f += temp;
+            lev -= temp;
+        }
+    return cur_flow - lev;
+}
+int dinic_flow(int s, int t){    //注意是从s到t，故而s最小，t最大
+    int sum = 0;
+    while(bfs(s, t)){
+        int flow;
+        for(int i = s; i <= t; i++) tpre[i] = pre[i];
+        while((flow = dfs(s, t, inf))) sum += flow;
+    }
+    return sum;
+}
+
+void flow_init(){
+    memset(pre, -1, sizeof(pre));
+    cnt = 0;
+}
+
+int limit_min_flow(int n, int m, int L, int l, int& flow, int &sum){
+    int src = 0, dest = n + m + 1;
+    for (int i = 1; i <= n; ++i) {
+        if (Time[i] == stTime[l]) {
+            addedge(m + i, dest, pay[i], 0); // n 个 plants 到 dest
+        }
+    }
+    
+    
+    for (int i = 1; i <= m; ++i) {
+        int ok = 1;
+        for (int j = 0; j < g[i].size(); ++j) { // 判断m个shop中，需要的plants全部在规定时间内
+            int t = Time[g[i][j]];
+            if (t > stTime[l]) {
+                ok = 0;
+                break;
+            }
+        }
+        if (ok && !vis[i]) {
+            vis[i] = 1;
+            addedge(src, i, pro[i], 0);
+            sum += pro[i];
+            for (int j = 0; j < g[i].size(); ++j) {
+                addedge(i, g[i][j] + m, inf, 0);
+            }
+        }
+    }
+    
+    flow += dinic_flow(src, dest);
+    return sum - flow >= L;
+}
+
+
+int main(){
+    
+    //freopen("in.txt","r",stdin);
+//    freopen("out.txt","w",stdout);
+    
+    
+    //freopen("in.txt","r",stdin);
+    //freopen("out.txt","w",stdout);
+    
+    int ca = 0, CASE;
+    cin >> CASE;
+    while(CASE--){
+        int n, m, L;
+        scanf("%d%d%d", &n, &m, &L);
+        for (int i = 1; i <= n; ++i) {
+            scanf("%d%d", &pay[i], &Time[i]);
+            stTime[i] = Time[i];
+        }
+        for (int i = 1; i <= m; ++i) {
+            scanf("%d", &pro[i]);
+            g[i].clear();
+            int k;
+            scanf("%d", &k);
+            while (k--) {
+                int tmp;
+                scanf("%d", &tmp);
+                g[i].push_back(tmp);
+            }
+        }
+        printf("Case #%d: ", ++ca);
+        flow_init();
+        sort(stTime + 1, stTime + 1 + n);
+        memset(vis, 0, sizeof(vis));
+        int ok = 0, l = 1, flow = 0, sum = 0;
+        while (l <= n) {
+            int r = l + 1;
+            while (r <= n && stTime[l] == stTime[r]) {
+                ++r;
+            }
+            if (limit_min_flow(n, m, L, l, flow, sum)) {
+                cout << stTime[l] << " " << sum - flow << endl;
+                ok = 1;
+                break;
+            }
+            else {
+                l = r;
+            }
+        }
+        if (!ok) {
+            cout << "impossible" << endl;
+        }
+        
+    }
+    return 0;
+}
