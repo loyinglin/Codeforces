757bbebccf8fcc2cea09f2bd0c2ae16a18fe2cf2
林伟池 <loying@foxmail.com>
2016-11-24 09:51:22 +0800

    模板+题解

--- 723/F/Codeforces/main.cpp
+++ 723/F/Codeforces/main.cpp
@@ -47,7 +47,15 @@
  （特殊考虑s、t直接相连的情况）
  
  
- 那么遍历所有s的边，如果这个边对应的连通块只与s相连
+ 那么遍历所有s的边，如果这个边对应的连通块只与s相连，直接dfs（不经过t）；
+ 类似解决情况2；
+ 
+ 现在问题在如果一个连通块与s和t相连，要考虑这个连通块是否可以经过t；
+ 于是先对t遍历，所有边先不经过s；
+ 
+ 最后对s的边遍历，如果发现没有与s相连，需要连一条s；
+ 特殊的情况，就是s和t相连；
+ 
  
  
  ************************* 题解 ***********************/
@@ -77,6 +85,8 @@ int con[N][2];
 int vis[N];
 int ans[N][2], ant = 0;
 
+map<pair<int, int>, int> Hash;
+
 void dfs(int u, int dep){
     low[u] = dfn[u] = dep;
     S.push(u);
@@ -105,6 +115,27 @@ int tarjan(int n){
     return id;
 }
 
+void addAns(int u, int v) {
+    ans[ant][0] = u;
+    ans[ant++][1] = v;
+    Hash[make_pair(u, v)] = 1;
+    Hash[make_pair(v, u)] = 1;
+}
+
+void look(int u) {
+    if (vis[u]) {
+        return ;
+    }
+    vis[u] = 1;
+    for (int i = 0; i < g[u].size(); ++i) {
+        int v = g[u][i];
+        if (!vis[v]) {
+            addAns(u, v);
+            look(v);
+        }
+    }
+}
+
 int main(int argc, const char * argv[]) {
     int n, m;
     cin >> n >> m;
@@ -124,73 +155,92 @@ int main(int argc, const char * argv[]) {
         int v = g[t][i];
         con[sv[v]][1] = 1;
     }
-    cout << id << endl;
-    
-    queue<int> q;
-    vis[sv[s]] = vis[sv[t]] = 1;
+    vis[s] = vis[t] = 1;
     for (int i = 0; i < g[s].size(); ++i) {
         int v = g[s][i];
         int k = sv[v];
-        if (con[k][0] == 1 && con[k][1] == 0) {
-            ans[ant][0] = s;
-            ans[ant][1] = v;
-            vis[k] = 1;
-            q.push(v);
-            ant++;
+        if (con[k][0] == 1 && con[k][1] == 0 && !vis[v]) {
+            --ds;
+            addAns(s, v);
+            look(v);
         }
     }
     for (int i = 0; i < g[t].size(); ++i) {
         int v = g[t][i];
         int k = sv[v];
-        if (con[k][0] == 0 && con[k][1] == 1) {
-            ans[ant][0] = t;
-            ans[ant][1] = v;
-            vis[k] = 1;
-            q.push(v);
-            ant++;
+        if (con[k][0] == 0 && con[k][1] == 1 && !vis[v]) {
+            --dt;
+            addAns(t, v);
+            look(v);
         }
-        else { // 两边都有
-            if (!vis[v]) { // 该点没有归属
-                
+    }
+    for (int i = 0; i < g[s].size(); ++i) {
+        int v = g[s][i];
+        int k = sv[v];
+        if (con[k][0] == 1 && con[k][1] == 1 && !vis[v]) { // 不经过t点的dfs
+            if (ds > 0) {
+                --ds;
+                addAns(s, v);
+                look(v);
             }
-            ans[ant][0] = t;
-            ans[ant][1] = v;
-            ant++;
-            if (!vis[v]) {
-                vis[v] = 1;
-                q.push(v);
+        }
+    }
+    for (int i = 0; i < g[t].size(); ++i) {
+        int v = g[t][i];
+        int k = sv[v];
+        if (con[k][0] == 1 && con[k][1] == 1 && !vis[v]) { // 不经过s点的dfs
+            if (dt > 0) {
+                --dt;
+                addAns(t, v);
+                look(v);
+            }
+        }
+    }
+    int ok = 0, k, *kp;
+    if (ds > dt) {
+        k = s;
+        kp = &ds;
+    }
+    else {
+        k = t;
+        kp = &dt;
+    }
+    for (int i = 0; i < g[k].size(); ++i) {
+        int v = g[k][i];
+        if (con[sv[v]][0] + con[sv[v]][1] == 2 && v != (s + t - k) && Hash[make_pair(k, v)] == 0) {
+            *kp = *kp - 1;
+            ok = 1;
+            addAns(k, v);
+            break;
+        }
+    }
+    if (!ok) {
+        for (int i = 0; i < g[s].size(); ++i) {
+            if (g[s][i] == t) {
+                --ds;
+                --dt;
+                addAns(s, t);
             }
         }
     }
     
-
-    
-    
-    
-    while (!q.empty()) {
-        int u = q.front();
-        q.pop();
-        for (int i = 0; i < g[u].size(); ++i) {
-            int v = g[u][i];
-            if (!vis[v]) {
-                ans[ant][0] = u;
-                ans[ant][1] = v;
-                ++ant;
-                vis[v] = 1;
-                q.push(v);
-            }
+    if (ant == n - 1 && ds >= 0 && dt >= 0) {
+        cout << "Yes" << endl;
+        for (int i = 0; i < n - 1; ++i) {
+            cout << ans[i][0] << " " << ans[i][1] << endl;
         }
     }
-    
+    else {
+        cout << "No" << endl;
+    }
     
     return 0;
 }
 
 /*
- 9 12
+ 9 11
  1 2
  1 8
- 2 3
  2 9
  3 4
  4 9
@@ -201,6 +251,79 @@ int main(int argc, const char * argv[]) {
  7 8
  8 9
  
- 9 1 1 1
+ 3 2 1 1
  
+ 
+ 
+ 
+ 
+ 4 5
+ 1 2
+ 2 3
+ 1 3
+ 1 4
+ 4 3
+ 
+ 1 3 1 2
+ 
+ 
+ 10 15
+ 4 1
+ 5 10
+ 2 1
+ 5 7
+ 9 2
+ 4 6
+ 6 7
+ 9 1
+ 6 9
+ 8 4
+ 8 3
+ 9 8
+ 3 9
+ 2 3
+ 7 10
+ 10 1 2 3
+ 
+ 
+ 
+ 6 8
+ 3 1
+ 3 2
+ 3 5
+ 3 6
+ 4 1
+ 4 2
+ 4 5
+ 4 6
+ 3 4 2 3
+ 
+ 
+ 4 3
+ 1 2
+ 2 3
+ 3 4
+ 2 3 2 2
+ 
+ 
+ 
+ 6 6
+ 1 2
+ 2 4
+ 2 3
+ 4 5
+ 3 5
+ 5 6
+ 2 5 4 1
+ 
+ 
+ 
+ 6 6
+ 1 2
+ 2 4
+ 2 3
+ 4 5
+ 3 5
+ 5 6
+ 1 4 1 1
  */
