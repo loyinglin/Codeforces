1a3105785216972e1b75de6990eb65bedd4329d3
林伟池 <loying@foxmail.com>
2017-04-16 16:39:27 +0800

    总结

--- 712/D/Codeforces/main.cpp
+++ 712/D/Codeforces/main.cpp
@@ -8,23 +8,23 @@
 /************************** 题解 **********************
  题目链接：http://codeforces.com/contest/712/problem/D
  题目大意：
- A 和 B玩一个游戏，初始时，A和B的分数为a和b。
- 每轮游戏，A和B都可以从[-k, k]的区间里面选择一个整数加到自己的分数。
+ A 和 B玩一个游戏，游戏分成t轮。
+ 初始时，A和B的分数为a和b；
+ 每轮游戏，A和B都可以从[-k, k]的区间里面选择一个整数加到自己的分数；
  问最后有多少种可能，A的分数大于B的分数。
  （只要某一轮A或者B的分数不同，即视为不同的可能）
   
  a, b, k, and t (1 ≤ a, b ≤ 100, 1 ≤ k ≤ 1000, 1 ≤ t ≤ 100)
  
- 结果要mod 1 000 000 007 (109 + 7)。
+ 结果要mod 1 000 000 007 (1e9 + 7)。
  
  input
  1 2 2 1
  output
  6
  
- Note
- In the first sample test, Memory starts with 1 and Lexa starts with 2. If Lexa picks  - 2, Memory can pick 0, 1, or 2 to win. If Lexa picks  - 1, Memory can pick 1 or 2 to win. If Lexa picks 0, Memory can pick 2 to win. If Lexa picks 1 or 2, Memory cannot win. Thus, there are 3 + 2 + 1 = 6 possible games in which Memory wins.
- 
+ 样例解释：
+ In the first sample test, A starts with 1 and B starts with 2. If B picks  - 2, A can pick 0, 1, or 2 to win. If B picks  - 1, A can pick 1 or 2 to win. If B picks 0, A can pick 2 to win. If B picks 1 or 2, A cannot win. Thus, there are 3 + 2 + 1 = 6 possible games in which A wins.
 
  
  
@@ -34,9 +34,7 @@
  接下来考虑去重，存在一轮分数不同，即是在第i轮的时候，选择不同的数字。
  那么，我们用dp[i][j]来表示 前i轮，分数等于j的选择个数。
  这样就可以统计出t轮之后，分数为j的不同选择个数。
- 
  先是对A  dp出t次后的结果，再对B dp出t次后的结果。这两个都是O(NK^2)。
- 
  最后再对dpB[n][j] 统计下所有∑dpA[n][c]，c > j，然后相乘，得到B为j的时候，A赢的选择数。
  最后累加j = [-k * k / 2, k * k / 2]的和即可。
  
