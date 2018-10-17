07f7e3934659703098fdbc995dd56b741d565d52
linweichi <linweichi@bytedance.com>
2018-10-17 21:51:42 +0800

    leetcode

--- leetcode/022 Generate Parentheses/ACM/main.cpp
+++ leetcode/022 Generate Parentheses/ACM/main.cpp
@@ -7,8 +7,31 @@
 /************************** 题解 **********************
  题目链接：https://leetcode.com/problems/generate-parentheses/description/
  题目大意：
+ 给出一个整数n，求n对括号组成的，所有可能的合法字符串；
+ 
+ 例如，n=3，则有：
+ [
+ "((()))",
+ "(()())",
+ "(())()",
+ "()(())",
+ "()()()"
+ ]
+ 
+ 
  
  题目解析：
+ 合法的字符串指的是左右括号数量相同，并且每一个左括号，都能在其右边找到一个右括号；
+ 类似")("这样就是不合法的字符串。
+ 理解定义之后，考虑长度为2*n的字符串中第i个字符应该怎么填：
+ 假设此刻已经具有的左括号有left个，右括号有right个；
+ 如果left>right，比如说"(()"，则可以放左括号=>"(()("，也可以放右括号=>"(())"
+ 如果left==right，比如说"(())"，那么只能放左括号=>"(())"
+ 如果left<right，此时为不合法序列，我们不应该出现这种情况；
+ 
+ 在此过程中，需要注意保证左右括号的数量不要超过n个；
+ 
+ 
  优化思路：
  
  
@@ -48,7 +71,6 @@ using namespace std;
 typedef long long lld;
 const int N = 50000;
 
-
 class Solution {
 public:
     
@@ -69,9 +91,16 @@ public:
             ret.push_back(str);
         }
         
-        d
+        
+    }
+    
+    vector<string> generateParenthesis(int n) {
+        vector<string> ret;
+        string tmp;
+        dfs(ret, tmp, n, n);
+        return ret;
+    }
 }leetcode;
-
 
 int main(int argc, const char * argv[]) {
     
